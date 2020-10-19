ce7f26624135c92f94fa3ea666b7af7d8abe6ca2
kevin <s_bo8dc2@uni-bremen.de>
2020-10-19 17:31:19 +0200

    remove giskard dependencies from odom_map_transform_publisher

--- script/odom_map_transform_publisher.py
+++ script/odom_map_transform_publisher.py
@@ -3,13 +3,9 @@ import rospy
 import numpy as np
 from geometry_msgs.msg import TransformStamped, Quaternion, Transform
 from tf2_msgs.msg import TFMessage
-from iai_naive_kinematics_sim.srv import SetMapOdomTransform, SetOdomMapTransformRequest, SetOdomMapTransformResponse 
-
-from giskardpy import logging
-from giskardpy.plugin import GiskardBehavior
-from giskardpy.utils import normalize_quaternion_msg
-
+from iai_naive_kinematics_sim.srv import SetMapOdomTransform
 from threading import Lock
+import numpy as np
 
 
 class OdomTransformPublisher(object):
@@ -17,10 +13,18 @@ class OdomTransformPublisher(object):
     def __init__(self, name):
         rospy.init_node(name)
         self.tf_pub = rospy.Publisher(u'/tf', TFMessage, queue_size=10)
+        while not rospy.has_param('~child_frame'):
+            logging.loginfo('waiting for param ' + '~child_frame')
+            rospy.sleep(1)
+        self.child_frame = rospy.get_param('~child_frame', "")
+        while not rospy.has_param('~parent_frame'):
+            logging.loginfo('waiting for param ' + '~parent_frame')
+            rospy.sleep(1)
+        self.parent_frame = rospy.get_param('~parent_frame', "")
         self.transform = Transform()
         self.transform.rotation = Quaternion(0, 0, 0, 1)
         self.rate = rospy.Rate(10)
-        self.service = rospy.Service('update_map_odom_transform', SetMapOdomTransform, self.update_transform_cb)
+        self.service = rospy.Service('~update_map_odom_transform', SetMapOdomTransform, self.update_transform_cb)
         self.lock = Lock()
         self.loop()
 
@@ -32,7 +36,7 @@ class OdomTransformPublisher(object):
     def update_transform_cb(self, req):
         with self.lock:
             self.transform = req.transform
-            self.transform.rotation = normalize_quaternion_msg(self.transform.rotation)
+            self.transform.rotation = self.normalize_quaternion_msg(self.transform.rotation)
         return True
 
     def publish_transform(self):
@@ -40,11 +44,24 @@ class OdomTransformPublisher(object):
         tf =  TransformStamped()
         with self.lock:
             tf.transform = self.transform
-        tf.header.frame_id = "map"
+        tf.header.frame_id = self.parent_frame
         tf.header.stamp = rospy.get_rostime()
-        tf.child_frame_id = "odom_combined"
+        tf.child_frame_id = self.child_frame
         tf_msg.transforms.append(tf)
         self.tf_pub.publish(tf_msg)
+
+    def normalize_quaternion_msg(self, quaternion):
+        q = Quaternion()
+        rotation = np.array([quaternion.x,
+                             quaternion.y,
+                             quaternion.z,
+                             quaternion.w])
+        normalized_rotation = rotation / np.linalg.norm(rotation)
+        q.x = normalized_rotation[0]
+        q.y = normalized_rotation[1]
+        q.z = normalized_rotation[2]
+        q.w = normalized_rotation[3]
+        return q
 
 
 if __name__ == "__main__":
